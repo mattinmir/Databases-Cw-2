26cf1918252a0fc2a8fc2c5ca01bbb17a4c0d8d8
mattinmir <mattinmir@users.noreply.github.com>
2015-02-12 20:28:49 +0000

    Update cw2.sql

--- cw2.sql
+++ cw2.sql
@@ -7,19 +7,55 @@ FROM person AS pa JOIN person AS pb ON pa.name = pb.mother AND pa.dod IS NOT NUL
 
 SELECT pa.name, pa.born_in
 FROM person AS pa JOIN person AS pb ON pb.name = pa.mother AND pa.born_in = pb.born_in
+
 ORDER BY pa.name;
 
 -- Q3 returns (name)
 
-; 
+SELECT father AS name
+FROM person
+UNION
+SELECT mother AS name
+FROM person
+
+ORDER BY name; 
 
 -- Q4 returns (name)
 
-;
+SELECT name
+FROM person
+WHERE gender='M'
+EXCEPT
+SELECT father 
+FROM person
+
+ORDER BY name;
 
 -- Q5 returns (name)
 
-;
+-- Returns all mothers with the genders of each of their children
+SELECT gender AS child_gender, mother
+FROM person
+WHERE mother IS NOT NULL
+ORDER BY mother
+-- Returns cross of all genders with all known mothers
+SELECT gender AS all_genders, mother
+FROM (SELECT DISTINCT mother FROM person) AS mother CROSS JOIN (SELECT DISTINCT gender FROM person) AS gender 
+WHERE mother IS NOT NULL 
+ORDER BY mother
+
+
+SELECT DISTINCT mother AS name
+FROM 	((SELECT gender AS child_gender, mother
+	FROM person
+	WHERE mother IS NOT NULL)
+
+	INTERSECT
+
+	(SELECT gender, mother
+	FROM (SELECT DISTINCT mother FROM person) AS mothers CROSS JOIN (SELECT DISTINCT gender FROM person) AS genders
+	WHERE mother IS NOT NULL )) AS MO_GEN;
+		
 
 -- Q6 returns (name,father,mother)
 
